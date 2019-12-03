9069447ff958ed922b205fd5aebedd0d07024cbc
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-12-02 22:46:51 -0500

    Simplify env variables. Set the right Google Analytics property ID.

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -7,13 +7,7 @@ module Jekyll
     # Sets the env variables that we need in the site configuration
     def generate(site)
       Dotenv.load
-      site.config['google_analytics'] = ENV['GOOGLE_ANALYTICS_ID']
-      site.config['disqus_shortname'] = ENV['DISQUS_SHORTNAME']
-      site.config['convert_kit_form_uid_mobile'] = ENV['CONVERT_KIT_FORM_UID_MOBILE']
-      site.config['convert_kit_js_code_mobile'] = ENV['CONVERT_KIT_JS_CODE_MOBILE']
-      site.config['convert_kit_page_link_mobile'] = ENV['CONVERT_KIT_PAGE_LINK_MOBILE']
-      site.config['convert_kit_form_uid_desktop'] = ENV['CONVERT_KIT_FORM_UID_DESKTOP']
-      site.config['convert_kit_js_code_desktop'] = ENV['CONVERT_KIT_JS_CODE_DESKTOP']
+      site.config["google_analytics"] = "UA-9318740-1"
     end
   end
 end
